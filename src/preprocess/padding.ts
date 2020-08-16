ff8fe673cbee76d161699513058dea52930f9ec2
Scott Lai <tuonglai3602@gmail.com>
2020-08-16 16:41:35 -0700

    Added padding.ts

--- src/preprocess/padding.ts
+++ src/preprocess/padding.ts
@@ -1,3 +1,63 @@
-export default function padding(message: string) {
+import { stdout } from 'process';
+import { clearTerminal } from 'ansi-escapes';
+import to64bits from "../utilities/to64bits";
+import sleep from '../utilities/sleep';
+import speed from '../utilities/speed';
+import title from '../utilities/title';
+import { yellow } from 'chalk';
 
+/**
+ * Returns the padded message.
+ * 
+ * @param message binary string
+ */
+export default function padding(message: string) {
+  if (!message.match(/^[0-1]{0, }$/g)) {
+    throw new Error('NOT A BINARY STRING ' + message);
+  }
+
+  let K = (512 - message.length - 1 - 64) % 512;
+
+  if (K < 0) {
+    K = (K + 512) % 512;
+  }
+
+  const L = to64bits(message.length);
+
+  return `${message}1${'0'.repeat(K)}${L}`;
+}
+
+export async function paddingAnimation(message: string) {
+  if (!message.match(/^[0-1]{0, }$/g)) {
+    throw new Error('NOT A BINARY STRING ' + message);
+  }
+
+  const padded = padding(message);
+
+  stdout.write(clearTerminal);
+  await sleep(speed.normal);
+  stdout.write(title(`padding ${message.length} -> ${padded.length}`));
+  await sleep(speed.normal);
+  stdout.write(message);
+  await sleep(speed.slow);
+  stdout.write(yellow`1`);
+
+  let K = (512 - message.length - 1 - 64) % 512;
+
+  if (K < 0) {
+    K = (K + 512) % 512;
+  }
+
+  for (let i = 0; i < K; i += 1) {
+    await sleep(speed.normal);
+    stdout.write('0');
+  }
+
+  await sleep(speed.slow);
+
+  const L = to64bits(message.length);
+
+  stdout.write(L);
+
+  return padded;
 }
