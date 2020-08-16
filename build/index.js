0e31011315afe680c20b248b9f0d475b0b324f2d
Scott Lai <tuonglai3602@gmail.com>
2020-08-16 16:21:09 -0700

    Updated .gitignore to include .js files

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-const process_1 = require("process");
-const toBinaryString_1 = require("./preprocess/toBinaryString");
-toBinaryString_1.toBinaryStringAnimation((_a = process_1.argv[2]) !== null && _a !== void 0 ? _a : 'abc');
