b80cb519126f7a125ac3f4eb5755f2e75d1bd546
Scott Lai <tuonglai3602@gmail.com>
2020-08-17 11:23:25 -0700

    Added hAnimation()

--- src/constants/H.ts
+++ src/constants/H.ts
@@ -1,3 +1,8 @@
+import { stdout } from 'process';
+import { clearTerminal } from 'ansi-escapes';
+import sleep from '../utilities/sleep';
+import speed from '../utilities/speed';
+
 /** First 8 prime numbers. */
 const primes = [2, 3, 5, 7, 11, 13, 17, 19];
 
@@ -13,5 +18,30 @@ const H = squareRootPrimes.map((squareRootPrime) => {
 
   return decimalPart.toString(2).substring(2, 34);
 });
+
+export async function hAnimation() {
+  stdout.write(clearTerminal);
+  await sleep(speed.medium);
+  stdout.write('initialize hash');
+  await sleep(speed.medium);
+
+  stdout.write(`h0 = √02 = ${H[0]}\n`);
+  await sleep(speed.medium);
+  stdout.write(`h1 = √03 = ${H[1]}\n`);
+  await sleep(speed.medium);
+  stdout.write(`h2 = √05 = ${H[2]}\n`);
+  await sleep(speed.medium);
+  stdout.write(`h3 = √07 = ${H[3]}\n`);
+  await sleep(speed.medium);
+  stdout.write(`h4 = √11 = ${H[4]}\n`);
+  await sleep(speed.medium);
+  stdout.write(`h5 = √13 = ${H[5]}\n`);
+  await sleep(speed.medium);
+  stdout.write(`h6 = √17 = ${H[6]}\n`);
+  await sleep(speed.medium);
+  stdout.write(`h7 = √19 = ${H[7]}`);
+  
+  return H;
+}
 
 export default H;
